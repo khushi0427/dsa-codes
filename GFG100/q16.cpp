1fa156c7709b3320e61548882c7a8cb821644d7e
khushi0427 <khushiraynn@gmail.com>
2025-09-20 01:19:25 +0530

    gfg questions ??s

--- GFG100/q16.cpp
+++ GFG100/q16.cpp
@@ -1,2 +1,14 @@
 // Write a Program to Check the Prime Number
 
+#include <iostream>
+using namespace std;
+bool primeCheck
+int main() {
+  int n;
+  cout<<"Enter the number:- "<<endl;
+  cin >>n;
+
+  primeCheck(n);
+
+    return 0;
+}
